8a5b035a33c97a1652f136777f707294669ba3ed
kesslerm <kesslerm@gmail.com>
2014-05-15 08:47:58 -0600

    Add logging of outside temperature with data

--- insert.php
+++ insert.php
@@ -3,26 +3,38 @@
 require 'inc/config.php';
 require 'inc/class.db.php';
 require 'collect.php';
+require 'yahoo-api-master/YahooWeather.class.php';
 
 try {
   $db = new DB($config);
+
+  $yahoo = new YahooWeather((int)$config["local_woeid"]);
+  $temperature = sprintf("%.02f", $yahoo->getTemperature(false));
+  //echo "Temperature:" . $temperature . "\n";
+  
+  
   if ($result = $db->res->query("SELECT id, serial FROM devices")) {
     while ($row = mysqli_fetch_row($result)) {
-  	$data = get_nest_data($row[1]);
-  	if (!empty($data['timestamp'])) {
-    		if ($stmt = $db->res->prepare("REPLACE INTO data (device_id, timestamp, heating, cooling, fan, autoAway, manualAway, leaf, target, target2, current, humidity, updated) " .
-		                              "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,NOW())")) {
-      		  $stmt->bind_param("isiiiiiidddi", $row[0], $data['timestamp'], $data['heating'], $data['cooling'], $data['fan'], $data['autoAway'], $data['manualAway'], $data['leaf'], 
-		                    $data['target_temp'], $data['target_temp2'],  $data['current_temp'], $data['humidity']);
-		  if (!$stmt->execute()) {
-		    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-		  }
-      		  $stmt->close();
-    	        }
-  	}
+      $data = get_nest_data($row[1]);
+      //print_r($data);
+      
+      if (!empty($data['timestamp'])) 
+      {
+        if ($stmt = $db->res->prepare("REPLACE INTO data (device_id, timestamp, heating, cooling, fan, autoAway, manualAway, leaf, target, target2, current, humidity, outside, updated) " .
+                                    "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW())")) 
+        {
+          $stmt->bind_param("isiiiiiidddii", $row[0], $data['timestamp'], $data['heating'], $data['cooling'], $data['fan'], $data['autoAway'], $data['manualAway'], $data['leaf'], 
+                          $data['target_temp'], $data['target_temp2'],  $data['current_temp'], $data['humidity'],$temperature);
+          if (!$stmt->execute()) 
+          {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+          }
+          $stmt->close();
+        }
+      }
     }
     mysqli_free_result($result);
-  }  
+  }
   $db->close();
 } catch (Exception $e) {
   $errors[] = ("DB connection error! <code>" . $e->getMessage() . "</code>.");
