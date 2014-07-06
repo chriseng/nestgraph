091d96529e6bc0705fd192dfdf0ea0fb487e648b
Christian Oyarzun <co@oyarzun.net>
2014-07-06 16:08:00 -0400

    Updated to use username and password as members instead of constants

--- collectEnergy.php
+++ collectEnergy.php
@@ -3,51 +3,49 @@
 require 'inc/config.php';
 require 'nest-api-master/nest.class.php';
 
-define('USERNAME', $config['nest_user']);
-define('PASSWORD', $config['nest_pass']);
-
 date_default_timezone_set($config['local_tz']);
 
 function get_nest_data($serial_number=null) {
-  $nest = new Nest();
-  $info = $nest->getDeviceInfo($serial_number);
+  global $config;
+  $nest = new Nest($config['nest_user'], $config['nest_pass']);
+  //$info = $nest->getDeviceInfo($serial_number);
   $energy = $nest->getEnergyLatest($serial_number);
-  
   //Change nulls to 0
-  foreach ($energy->days as &$day)
+  foreach($energy->objects as &$object)
   {
-    foreach ($day->events as &$events)
+     $value = $object->value;
+    foreach ($value->days as &$day)
     {
-      if ( empty($events -> continuation) )
+      foreach ($day->events as &$events)
       {
-        $events -> continuation = 0;
+        if ( empty($events -> continuation) )
+        {
+          $events -> continuation = 0;
+        }
+        if ( empty($events -> touched_by) )
+        {
+          $events -> touched_by = 0;
+        }
+        if ( empty($events -> touched_when) )
+        {
+          $events -> touched_when = 0;
+        }
+        if ( empty($events -> touched_timezone_offset) )
+        {
+          $events -> touched_timezone_offset = 0;
+        }
+        if ( empty($events -> touched_where) )
+        {
+          $events -> touched_where = 0;
+        }
+        $events -> heat_temp = $nest->temperatureInUserScale($events -> heat_temp);
+        $events -> cool_temp = $nest->temperatureInUserScale($events -> cool_temp);
       }
-      if ( empty($events -> touched_by) )
-      {
-        $events -> touched_by = 0;
-      }
-      if ( empty($events -> touched_when) )
-      {
-        $events -> touched_when = 0;
-      }
-      if ( empty($events -> touched_timezone_offset) )
-      {
-        $events -> touched_timezone_offset = 0;
-      }
-      if ( empty($events -> touched_where) )
-      {
-        $events -> touched_where = 0;
-      }
+      unset($events);
     }
-    unset($events);
+    unset($day);
   }
-  unset($day);
-  
+  unset($object);
   return $energy;  
 }
-
-function c_to_f($c) {
-  return ($c * 1.8) + 32;
-}
-
 ?>
--- init_devices.php
+++ init_devices.php
@@ -4,11 +4,9 @@ require 'inc/config.php';
 require 'inc/class.db.php';
 require 'nest-api-master/nest.class.php';
 
-define('USERNAME', $config['nest_user']);
-define('PASSWORD', $config['nest_pass']);
 date_default_timezone_set($config['local_tz']);
 
-$nest = new Nest();
+$nest = new Nest($config['nest_user'], $config['nest_pass']);
 try {
 $db = new DB($config);
 $devices= $nest->getDevices();
